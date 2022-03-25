9d919d1348f354b62595fe0c50fd102fec2b584a
Kevin-- <kevin.oliveira@pontaltelecom.com.br>
2022-03-24 22:41:56 -0300

    .pet command

--- app/controller/kappashiro.ts
+++ app/controller/kappashiro.ts
@@ -1,10 +1,11 @@
 import {
     logger
 } from '../lib/logger';
-import * as Discord from 'discord.js';
-const client = new Discord.Client();
 import * as fs from 'fs';
+import * as petPetGif from 'pet-pet-gif';
 import axios from 'axios';
+import * as Discord from 'discord.js';
+const client: any = new Discord.Client();
 // import rabbit from '../lib/rabbitmq';
 
 const kappashiro = {
@@ -20,14 +21,14 @@ const kappashiro = {
                 })
             });
 
-            let statuses = [{
+            let statuses: any = [{
                 activity: {
                     name: `${process.env.PREFIX}help`
                 },
                 type: "PLAYING"
             }];
-            let i = 0;
-            let status = statuses[i];
+            let i: number = 0;
+            let status: any = statuses[i];
 
             setInterval(async () => {
                 if (!status) {
@@ -38,25 +39,48 @@ const kappashiro = {
                 i++;
             }, 1000 * 60 * 60);
 
-            client.on('message', async message => {
+            client.on('message', async (message: {
+                content: string;author: {
+                    bot: any;
+                };channel: {
+                    send: (arg0: string | Discord.MessageAttachment) => void;type: string;bulkDelete: (arg0: number, arg1: boolean) => any;
+                };reply: (arg0: string) => any;mentions: {
+                    users: {
+                        first: () => any;
+                    };members: {
+                        first: () => any;
+                    };
+                };member: {
+                    voice: {
+                        channel: any;
+                    };
+                };
+            }) => {
                 // await rabbit.send(`${process.env.APP}`, `${message}`);
 
                 if (!message.content.startsWith(process.env.PREFIX) || message.author.bot) return;
 
                 // rabbit.consume(process.env.APP, Number(process.env.CONCURRENCY), (message: any) => {}
 
-                const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
-                const command = args.shift().toLowerCase();
+                const args: any = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
+                const command: string = args.shift().toLowerCase();
 
                 if (command === 'ping') {
-                    await message.channel.send('pong');
+                    message.channel.send('pong');
 
                 } else if (command === 'help') {
                     logger.info(`help by ${message.author}`);
-                    await message.channel.send('work-in-progress');
+                    message.channel.send(`
+                bot made by: 🍪 𝑲𝒆𝒗𝒊𝒏 𝒍 𝑻𝒊𝒏𝒆𝒔𝒉#6426
+            commands:
+            **.purge <number>** - Command to delete messages between the value 1 to 100
+            **.avatar <user>** - Command to show someone's profile picture
+            **.pet <user>** - Command to show your affection to someone
+            **.play <number>** - Work in progress
+                `);
 
                 } else if (command === 'purge') {
-                    const amount = parseInt(args[0]);
+                    const amount: number = parseInt(args[0]);
                     console.log(`amount: ${amount}`);
                     if (isNaN(amount)) {
                         return await message.reply('Invalid value');
@@ -66,48 +90,58 @@ const kappashiro = {
                     if (message.channel.type === 'text') await message.channel.bulkDelete(amount, true)
 
                 } else if (command === 'avatar') {
-                    let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
-                    let avatar = user.avatarURL({
+                    let user: any = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
+                    let avatar: any = user.avatarURL({
                         dynamic: true,
                         format: "png",
                         size: 1024
                     });
-                    let embed = new Discord.MessageEmbed()
+                    let embed: any = new Discord.MessageEmbed()
                         .setColor(`#00000`)
                         .setTitle(`Avatar from ${user.username}`)
                         .setImage(avatar);
-                    await message.channel.send(embed);
+                    message.channel.send(embed);
+
+                } else if (command === 'pet') {
+                    const member: any = message.mentions.members.first() || message.member;
+                    let avatar: string = member.user.displayAvatarURL({
+                        format: "jpg"
+                    });
+                    let animatedGif: string = await petPetGif(avatar);
+                    message.channel.send(new Discord.MessageAttachment(animatedGif, 'pet.gif'));
 
                 } else if (command === 'play') {
-                    let VC = message.member.voice.channel;
+                    let VC: any = message.member.voice.channel;
                     if (!VC) {
                         return await message.reply("You are not connected to the voice channel")
                     }
                     await VC.join()
-                        .then(async connection => {
+                        .then(async (connection: {
+                            play: (arg0: string) => any;
+                        }) => {
                             console.log(`file: ${args[0]}`);
                             if (args[0] === 'random') {
-                                const playfile = fs.createWriteStream(`app/resources/media/audios/random.mp3`);
+                                const playfile: any = fs.createWriteStream(`app/resources/media/audios/random.mp3`);
                                 await axios({
                                     method: 'get',
                                     url: `${process.env.MYINSTANTS_URL}/?type=file`,
                                     responseType: 'stream',
                                 }).then(async response => {
                                     response.data.pipe(playfile);
-                                    const dispatcher = connection.play(`app/resources/media/audios/random.mp3`);
+                                    const dispatcher: any = connection.play(`app/resources/media/audios/random.mp3`);
                                     dispatcher.on("finish", (end: any) => {
                                         // VC.leave()
                                     });
                                 });
                             } else {
-                                const playfile = fs.createWriteStream(`app/resources/media/audios/play.mp3`);
+                                const playfile: any = fs.createWriteStream(`app/resources/media/audios/play.mp3`);
                                 await axios({
                                     method: 'get',
                                     url: `${process.env.MYINSTANTS_URL}/?type=file&id=${args[0]}`,
                                     responseType: 'stream',
                                 }).then(async response => {
                                     response.data.pipe(playfile);
-                                    const dispatcher = connection.play(`app/resources/media/audios/play.mp3`);
+                                    const dispatcher: any = connection.play(`app/resources/media/audios/play.mp3`);
                                     dispatcher.on("finish", (end: any) => {
                                         // VC.leave()
                                     });
