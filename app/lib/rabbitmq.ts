42e79159ee595314f8b4afdf9e0a372c9aecdb6a
71Kevin <kevinmanhas@gmail.com>
2023-03-20 02:26:16 -0300

    refactor/generalrefactoring

--- app/lib/rabbitmq.ts
+++ app/lib/rabbitmq.ts
@@ -1,73 +1,101 @@
-import { logger } from '../lib/logger';
 import * as amqp from 'amqplib';
+import { logger } from '../lib/logger';
 
-export default {
-  connection: null as amqp.Connection | null,
-  queues: null as amqp.ConfirmChannel | null,
+interface QueueMessage {
+  content: Buffer;
+}
 
-  async connect() {
+class RabbitMqService {
+  private connection: amqp.Connection | null = null;
+  private channel: amqp.Channel | null = null;
+
+  async connect(): Promise<void> {
+    const uri = `amqp://${process.env.RABBIT_USER}:${process.env.RABBIT_PASS}@${process.env.RABBIT_HOST}:${process.env.RABBIT_PORT}`;
     try {
-      const rabbitUri = `amqp://${process.env.RABBIT_USER}:${process.env.RABBIT_PASS}@${process.env.RABBIT_HOST}:${process.env.RABBIT_PORT}`;
-      this.connection = await amqp.connect(rabbitUri);
-      this.queues = await this.connection.createConfirmChannel();
-    } catch (e) {
-      logger.error(`connection on rabbitMq failed ${e.message} ${e.stack}`, e, {
+      this.connection = await amqp.connect(uri);
+      this.channel = await this.connection.createConfirmChannel();
+    } catch (error: any) {
+      logger.error(`Failed to connect to RabbitMQ: ${error.message}`, {
         severity: 'critical',
-        error: e.message,
+        error: error.message,
       });
+      throw error;
     }
-  },
+  }
 
-  async assertQueue(queueId: string) {
+  async assertQueue(queueName: string): Promise<amqp.Replies.AssertQueue> {
     try {
-      const currentQueue = await this.queues.assertQueue(queueId, {
-        durable: true,
-      });
-      return currentQueue;
-    } catch (e) {
-      logger.error(`queueId - ${queueId} - error to assertQueue ${e.message} ${e.stack}`, e, {
+      const queue = await this.channel!.assertQueue(queueName, { durable: true });
+      return queue;
+    } catch (error: any) {
+      logger.error(`Failed to assert queue ${queueName}: ${error.message}`, {
         severity: 'critical',
-        error: e.message,
+        error: error.message,
       });
-      throw e;
+      throw error;
     }
-  },
+  }
 
-  consume(
-    queueId: string,
-    concurrency: number,
-    callback: (msg: amqp.ConsumeMessage | null) => void,
-  ) {
-    this.destroy(queueId);
-    this.concurrency(concurrency);
-    this.queues.consume(queueId, callback, {
-      consumerTag: queueId,
-      noAck: false,
-    });
-  },
-
-  concurrency(quantity: number) {
-    return this.queues.prefetch(quantity, true);
-  },
-
-  ack(message: amqp.Message) {
-    return this.queues.ack(message);
-  },
-
-  destroy(queueId: string) {
-    return this.queues.cancel(queueId);
-  },
-
-  async send(queueId: string, message: string) {
+  async consume(
+    queueName: string,
+    onMessage: (message: QueueMessage) => Promise<void>,
+    concurrency = 1,
+  ): Promise<amqp.Replies.Consume> {
     try {
-      await this.queues.sendToQueue(queueId, Buffer.from(message), {});
-      logger.info(`sent: ${message}`);
-    } catch (e) {
-      logger.error(`queueId - ${queueId} - error to send message ${e.message} ${e.stack}`, e, {
-        severity: 'critical',
-        error: e.message,
+      await this.channel!.prefetch(concurrency, true);
+      const { consumerTag } = await this.channel!.consume(queueName, async (message) => {
+        if (message) {
+          try {
+            await onMessage({ content: message.content });
+            this.channel!.ack(message);
+          } catch (error: any) {
+            logger.error(`Failed to consume message: ${error.message}`, {
+              severity: 'error',
+              error: error.message,
+            });
+            this.channel!.nack(message);
+          }
+        }
       });
-      throw e;
+      return { consumerTag };
+    } catch (error: any) {
+      logger.error(`Failed to start consumer on queue ${queueName}: ${error.message}`, {
+        severity: 'critical',
+        error: error.message,
+      });
+      throw error;
     }
-  },
-};
+  }
+
+  async send(queueName: string, message: string): Promise<void> {
+    try {
+      await this.channel!.sendToQueue(queueName, Buffer.from(message), {});
+      logger.info(`Sent message to queue ${queueName}: ${message}`);
+    } catch (error: any) {
+      logger.error(`Failed to send message to queue ${queueName}: ${error.message}`, {
+        severity: 'critical',
+        error: error.message,
+      });
+      throw error;
+    }
+  }
+
+  async disconnect(): Promise<void> {
+    try {
+      if (this.channel) {
+        await this.channel.close();
+      }
+      if (this.connection) {
+        await this.connection.close();
+      }
+    } catch (error: any) {
+      logger.error(`Failed to disconnect from RabbitMQ: ${error.message}`, {
+        severity: 'error',
+        error: error.message,
+      });
+      throw error;
+    }
+  }
+}
+
+export default new RabbitMqService();
